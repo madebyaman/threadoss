f4cda40def1150ca9278168871612615d035e7bd
Aman Thakur <builtbyaman@gmail.com>
2023-01-16 18:24:48 +0530

    added auth0

--- pages/app/articles/index.tsx
+++ pages/app/articles/index.tsx
@@ -1,4 +1,5 @@
 import UI from '@/components/UI';
+import { PlusIcon } from '@heroicons/react/24/outline';
 
 export default function Articles() {
   const articles = [
@@ -26,18 +27,21 @@ export default function Articles() {
       <div className="font-sans">
         <div className="sm:flex sm:items-center">
           <div className="sm:flex-auto">
-            <h1 className="text-xl font-semibold text-gray-900">Users</h1>
+            <h1 className="text-xl font-semibold text-gray-900">
+              Recent Articles
+            </h1>
             <p className="mt-2 text-sm text-gray-700">
-              A list of all the users in your account including their name,
-              title, email and role.
+              A list of all the articles in your account including their title,
+              url and date added.
             </p>
           </div>
           <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
             <button
               type="button"
-              className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+              className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto gap-2"
             >
-              Add user
+              <PlusIcon className="h-4 w-4 text-gray-200" aria-hidden="true" />
+              New article
             </button>
           </div>
         </div>
@@ -50,19 +54,19 @@ export default function Articles() {
                     <tr>
                       <th
                         scope="col"
-                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                        className="px-4 py-3.5 text-left text-sm font-semibold text-gray-900"
                       >
                         Title
                       </th>
                       <th
                         scope="col"
-                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                        className="px-4 py-3.5 text-left text-sm font-semibold text-gray-900"
                       >
                         URL
                       </th>
                       <th
                         scope="col"
-                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                        className="px-4 py-3.5 text-left text-sm font-semibold text-gray-900"
                       >
                         Date Added
                       </th>
@@ -70,22 +74,22 @@ export default function Articles() {
                         scope="col"
                         className="relative py-3.5 pl-3 pr-4 sm:pr-6"
                       >
-                        <span className="sr-only">Edit</span>
+                        <span className="sr-only">Generate new thread</span>
                       </th>
                     </tr>
                   </thead>
                   <tbody className="divide-y divide-gray-200 bg-white">
                     {articles.map((article) => (
                       <tr key={article.title}>
-                        <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                        <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:px-6">
                           {article.title.length > 75
                             ? article.title.substring(0, 75) + '...'
                             : article.title}
                         </td>
-                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                        <td className="whitespace-nowrap px-4 py-4 text-sm text-gray-500">
                           {article.url}
                         </td>
-                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                        <td className="whitespace-nowrap px-4 py-4 text-sm text-gray-500">
                           {article.dateAdded.toLocaleDateString()}
                         </td>
                         <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
@@ -93,14 +97,39 @@ export default function Articles() {
                             href="#"
                             className="text-indigo-600 hover:text-indigo-900"
                           >
-                            Edit
-                            <span className="sr-only">, {article.title}</span>
+                            Regenerate thread
                           </a>
                         </td>
                       </tr>
                     ))}
                   </tbody>
                 </table>
+                <nav
+                  className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6"
+                  aria-label="Pagination"
+                >
+                  <div className="hidden sm:block">
+                    <p className="text-sm text-gray-700">
+                      Showing <span className="font-medium">1</span> to{' '}
+                      <span className="font-medium">10</span> of{' '}
+                      <span className="font-medium">20</span> results
+                    </p>
+                  </div>
+                  <div className="flex-1 flex justify-between sm:justify-end">
+                    <a
+                      href="#"
+                      className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+                    >
+                      Previous
+                    </a>
+                    <a
+                      href="#"
+                      className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+                    >
+                      Next
+                    </a>
+                  </div>
+                </nav>
               </div>
             </div>
           </div>
